12050f370beacff75de2646b20d2446c126a4990
GreenLantern16 <greg.thomas@hashicorp.com>
2021-12-13 08:39:32 -0500

    demo re-write start from scratch

--- infrastructure-as-code/aws-ec2-instance/main.tf
+++ infrastructure-as-code/aws-ec2-instance/main.tf
@@ -1,17 +1,5 @@
-terraform {
-  required_version = ">= 0.11.0"
+module "vpc_example_complete-vpc" {
+  source  = "terraform-aws-modules/vpc/aws//examples/complete-vpc"
+  version = "3.11.0"
 }
-
-provider "aws" {
-  region = "${var.aws_region}"
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = "${var.ami_id}"
-  instance_type = "${var.instance_type}"
-  availability_zone = "${var.aws_region}a"
-
-  tags {
-    Name = "${var.name}"
-  }
-}
+ 
